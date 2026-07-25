a43d74dd15fb1eed4420a313b9ac3732f3562d19
Arush Kumar <arushkumarbjn123@gmail.com>
2026-07-25 13:11:01 +0530

    static component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Card from "@/components/Card"
 
 export default function Home() {
   return (
     <div>
       <h1>Hello world!</h1>
+      <Card />
     </div>
   );
 }
---
+++ components/Card.tsx
@@ -0,0 +1,10 @@
+
+export default function Card() {
+  return (
+    <div className=" bg-amber-600 w-40 h-30 rounded-2xl m-4">
+      <h1 className="p-4 text-2xl">Card 1</h1>
+      <p className="p-4">This a card</p>
+    </div>
+  )
+}
+
