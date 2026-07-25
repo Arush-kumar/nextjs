4daa65155fd315eddaa9bcfd40b52a52055ed918
Arush Kumar <arushkumarbjn123@gmail.com>
2026-07-25 13:53:32 +0530

    client component ( CSR )

--- app/classroom/page.tsx
+++ app/classroom/page.tsx
@@ -1,8 +1,11 @@
+import Button from "@/components/Button";
+
 export default function Classroom() {
   return (
     <div>
       <h1 className="text-3xl">Classroom</h1>
       <p>This is Classroom page.</p>
+      <Button />
     </div>
   );
 }
